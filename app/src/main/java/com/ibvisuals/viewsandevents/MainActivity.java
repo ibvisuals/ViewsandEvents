3bb680fe69478f09f2f256065f378f1b1b05179c
Intyaud Banton <65741007+ibvisuals@users.noreply.github.com>
2021-02-18 19:32:31 -0500

    Initial commit

---
+++ app/src/main/java/com/ibvisuals/viewsandevents/MainActivity.java
@@ -0,0 +1,34 @@
+package com.ibvisuals.viewsandevents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView t1;
+    Button b1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        t1 = (TextView)findViewById(R.id.t1);
+        b1 = (Button)findViewById(R.id.b1);
+
+        b1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        t1.setText("You clicked Button 1");
+
+    }
+
+    public void doSomething(View view) {
+        t1.setText("You clicked Button 2");
+    }
+}
